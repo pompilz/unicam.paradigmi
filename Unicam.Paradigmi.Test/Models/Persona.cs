e3d7a5df7cd68ef2acf26fa09e5ec58dc076deae
LORENZO POMPILI <lorenzo.pompili@studenti.unicam.it>
2024-03-05 17:44:37 +0100

    Primo commit

---
+++ Unicam.Paradigmi.Test/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicam.Paradigmi.Test.Models
+{
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Età { get; set; }
+    }
+}
---
+++ Unicam.Paradigmi.Test/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+//REFERENZIO I NAMESPACE O CON LO USING O LA CLASSE SPECIFICA CON IL NOME COMPLETO
+//using Unicam.Paradigmi.Test.Models;
+
+using System.Collections;
+using System.Collections.Concurrent;
+using Unicam.Paradigmi.Test.Models;
+
+var persona = new Unicam.Paradigmi.Test.Models.Persona();
+persona.Nome = "Lorenzo";
+persona.Cognome = "Pompili";
+persona.Età = 21;
+//************************************************//
+
+//DICHIARAZIONE DI UN ARRAY
+
+string[] stagioni = new string[4];
+
+stagioni[0] = "Primavera";
+stagioni[1] = "Estate";
+stagioni[2] = "Autunno";
+stagioni[3] = "Inverno";
+foreach(var stagione in stagioni)
+{
+    Console.WriteLine($"Ciclo sulla stagione {stagione}");
+}
+//*********************************************************
+
+//PROVA PASSAGGIO PER VALORE / RIFERIMENTO
+var miaEtà = 15;
+var nome = "Lorenzo";
+var cognome = "Pompili";
+ModificaValore(miaEtà);
+Console.WriteLine($"Ciao mi chiamo {nome} {cognome}. La mia età è : {miaEtà}");
+ModificaValorePerRiferimento(ref miaEtà);
+Console.WriteLine($"Ciao mi chiamo {nome} {cognome}. La mia età è : {miaEtà}");
+int annoNascità = 0;
+IsMaggiorenne(miaEtà, out annoNascità);
+Console.WriteLine($"Sono nato nell'anno {annoNascità}");
+//Console.ReadLine();
+
+void ModificaValore(int età)
+{
+    età = 20;
+}
+
+void ModificaValorePerRiferimento(ref int età)
+{
+    età = 20;
+}
+
+bool IsMaggiorenne(int età, out int annoNascità)
+{
+    annoNascità = DateTime.Now.Year - età;
+    return età >= 18;
+}
+//*********************************************//
+
+
+//GESTIONE DI EVENTI//
+ArrayList listaArticoli = CreaListaArticoli();
+
+foreach(Articolo articolo in listaArticoli)
+{
+    articolo.QtaMagazzino -= 8;
+}
+
+ArrayList CreaListaArticoli()
+{
+    var lista = new ArrayList();
+    for (int i = 0; i < 10; i++)
+    {
+        var articolo = new Articolo();
+        articolo.CodiceArticolo = $"CODART{i:000}";
+        articolo.Descrizione = $"DESCART{i:000}";
+        Random rdm = new Random(30);
+        articolo.QtaMagazzino = rdm.Next(10, 20);
+        articolo.LivelloScortaMinima = articolo.QtaMagazzino / 2;
+        articolo.SottoScortaEvent += Articolo_SottoScortaEvent;
+        lista.Add(articolo);
+    }
+    return lista;
+}
+
+void Articolo_SottoScortaEvent(Articolo art, DateTime time)
+{
+    Console.WriteLine($"L'articolo {art.CodiceArticolo} è andato sotto scorta. QtaMagazzino : {art.QtaMagazzino} Livello Scorta Minimo : {art.LivelloScortaMinima}" );
+}
+
+//*********************************************//
